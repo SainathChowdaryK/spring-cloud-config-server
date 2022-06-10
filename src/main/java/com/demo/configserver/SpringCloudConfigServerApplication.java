f5f1fe7040e4f6e425a6a7b0eeda77f80c1a7a6a
SainathChowdaryK <sainathchowdary666@gmail.com>
2022-06-10 18:10:23 +0530

    Initial Commit

---
+++ src/main/java/com/demo/configserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
